59c7d7e25a8f2904886cf1066e3f6768531a74f8
Akanalanka Gajasinghe <angajasinghe@outlook.com>
2020-11-16 19:56:58 +0530

    Authentication basics with cookie

---
+++ Basic/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basic.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret() 
+        {
+            return View();
+        }
+
+        public IActionResult Autenticate()
+        {
+            //Creating claims for a user
+            var testClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"Bob"),
+                new Claim(ClaimTypes.Role,"Admin"),
+                new Claim(ClaimTypes.DateOfBirth,"19981105"),
+                new Claim(ClaimTypes.Email,"bob@bobMail.com"),
+                new Claim("Test User Says","You Can Go!"),
+            };
+
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, "bob@bobMail.com"),
+                new Claim(ClaimTypes.Name, "Bob Salmose"),
+                new Claim("DivingLicense","A+"),
+            };
+
+            //Add identity for a Claim
+            var testClaimIdentity = new ClaimsIdentity(testClaims, "Test Identity");
+            var licenseClaimIdentity = new ClaimsIdentity(licenseClaims, "Governtment");
+            //Add identity principle
+            var userPrinciple = new ClaimsPrincipal(new[] { testClaimIdentity , licenseClaimIdentity });
+
+
+            //Add user principle to the contex 
+            HttpContext.SignInAsync(userPrinciple);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Basic/Startup.cs
+++ Basic/Startup.cs
@@ -12,28 +12,44 @@ namespace Basic
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
         public void ConfigureServices(IServiceCollection services)
         {
+            // This is Cookie handler
+            // Tells application to who we are 
+            // Inject to the app.UseAuthentication(); method
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth" , configureOptions=> 
+                {
+                    configureOptions.Cookie.Name = "Nayanajith.Cookie";
+                    configureOptions.LoginPath = "/Home/Autenticate";
+                });
+
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
+
+
+
+            // who are you?
+            app.UseAuthentication();
+
+            // Are You Allowed?
+            app.UseAuthorization();
+       
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
